df00aab1553014b70854e13626b3677b621b4793
BuildTools <unconfigured@null.spigotmc.org>
2021-06-30 13:31:09 -0400

    Forget Packets

--- src/main/java/com/NetUserGet/XYZ/XYZ.java
+++ src/main/java/com/NetUserGet/XYZ/XYZ.java
@@ -4,10 +4,13 @@ package com.NetUserGet.XYZ;
 import com.NetUserGet.XYZ.config.XYZConfig;
 import com.NetUserGet.XYZ.hud.XYZHud;
 
+import io.netty.channel.ChannelHandler;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,6 +28,9 @@ public class XYZ implements ClientModInitializer {
 
 	public static final String LOGGER_PREFIX = "["+ MOD_ID.toUpperCase() +"] ";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+	public static Identifier channelName;
+	public static ClientPlayNetworking.PlayChannelHandler channelHandler;
+	private ClientPlayNetworking PlayClientNetworking;
 
 
 	@Override
--- src/main/java/com/NetUserGet/XYZ/packet/README.txt
+++
@@ -1,2 +0,0 @@
-# Before saying this is Hack/Advantage this is NOT used for the purpose to give you a competitive advantage!
-# This packet package is for Some Main Features.
